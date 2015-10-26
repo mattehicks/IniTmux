80b5c7b6a0bcb11346897c64f36668698f0fbffa
Itamar de Aguiar <itamar.de.aguiar@gmail.com>
2015-10-25 21:39:30 -0300

    Models Support

--- README.md
+++ README.md
@@ -1,44 +1,88 @@
 
-# Syntax
+# What is This Program?
+
+This is just a session store/initializer for Tmux.
+
+# How It Works?
+
+It's simple. All you have to do is plan your sessions into YAML files, put 
+then into `~/.config/IniTmux` and execute a single python script `src/main.py`.
+
+# How Do I Plan My Sessions?
+
+Let's say that you want to acompplish the following workspace:
+
+```text
+     Session: Files
+
+     Window: Musics
+     Layout: even-vertical
+     +-------------------+-------------------+
+     |                   |                   |
+     |                   |                   |
+     |                   |                   |
+     |                   |                   |
+     |                   |                   |
+     |      mocp .       |      ranger       |
+     |   (~/Musics)      |    (~/Musics)     |
+     |                   |                   |
+     |                   |                   |
+     |                   |                   |
+     |                   |                   |
+     |                   |                   |
+     |                   |                   |
+     +-------------------+-------------------+
+
+     Window: Downloads
+     Layout: even-horizontal
+     +---------------------------------------+
+     |                                       |
+     |               ranger                  |
+     |            (~/Downloads)              |
+     |                                       |
+     |                                       |
+     |                                       |
+     +---------------------------------------+
+     |                                       |
+     |            watch du -sh .             |
+     |            (~/Downloads)              |
+     |                                       |
+     |                                       |
+     |                                       |
+     +---------------------------------------+
+```
+
+So, this figure suggests that you want to create a session called `Files`, where 
+this session has two windows called `Musics` and `Downloads`, respectively. Whithin
+the window `Musics`, you want two panes. The first one will execute `mocp .` and the 
+second one will execute the file manager `ranger`, and both panes will be organized 
+under layout `even-vertical`. For the second window, the thinking is analogous.
+
+Hence, you have to write the YAML file (`Files.yml`) like the following:
 
 ```yaml
-- SessionName1:
-    root: /path/to/project/root     # Without last '/'
-    windows:
-        - WindowName1:
-            layout: tiled           # One of tmux layouts
-            dir: <subdir>           # It'll be prefixed with root
-            panes:
-                - cmd1
-                - cmd2
-                - cmd3
-        - WindowName2:
-            layout: tiled           # One of tmux layouts
-            dir: <subdir>           # It'll be prefixed with root
-            panes:
-                - cmd1
-                - ''                # Do nothing
-                - cmd3
-- SessionName2:
-    root: /path/to/project/root     # Without last '/'
-    windows:
-        - WindowName1:
-            layout: tiled           # One of tmux layouts
-            dir: <subdir>           # It'll be prefixed with root
-            panes:
-                - cmd1
-                - cmd2
-                - cmd3
-        - WindowName2:
-            layout: tiled           # One of tmux layouts
-            dir: <subdir>           # It'll be prefixed with root
-            panes:
-                - cmd1
-                - ''                # Do nothing
-                - cmd3
+---
+name: Files
+root: ~      # Do not put the last slash ('/')
 
+windows:
+- [Musics    , musics]
+- [Downloads , downloads]
+
+models
+    musics:
+        layout: even-vertical
+        dir: <WindowName> 
+        panes:
+        - mocp .
+        - ranger
+    downloads:
+        layout: even-horizontal
+        dir: <WindowName> 
+        panes:
+        - ranger
+        - watch du -sh .
 ```
-and so on.
 
 # Directory Location
 
--- src/main.py
+++ src/main.py
@@ -11,17 +11,20 @@ try:
 except ImportError:
     from yaml import Loader, Dumper
 
-def NewSession(SName):
+# Tmux Functions
+def NewSession(SName): #{{{
     return call('tmux new-session -s '+re.escape(SName)+' -d',shell=True)
 
 
-def NewWindow(SName,WName,WNumber):
+#}}}
+def NewWindow(SName,WName,WNumber): #{{{
     if WNumber > 0:
         return call('tmux new-window -n '+WName+' -t '+SName,shell=True)
     else:
         return call('tmux rename-window -t '+re.escape(SName)+':0 '+WName,shell=True)
 
-def NewPane(SName,WNumber,PNumber,Command,Directory):
+#}}}
+def NewPane(SName,WNumber,PNumber,Command,Directory): #{{{
     ret = True
     if PNumber > 0:
         ret = call('tmux split-window -h -t '+re.escape(SName)+':'+str(WNumber),shell=True)
@@ -35,76 +38,138 @@ def NewPane(SName,WNumber,PNumber,Command,Directory):
     return ret
 
 
-def ChangeDir(SName,WNumber,PNumber,Directory):
+#}}}
+def ChangeDir(SName,WNumber,PNumber,Directory): #{{{
     return call(
             'tmux send-keys -t '+
             re.escape(SName)+':'+str(WNumber)+'.'+str(PNumber)+
             ' "cd '+Directory+'" C-m',shell=True)
 
-def ApplyCommand(SName,WNumber,PNumber,Command):
+#}}}
+def ApplyCommand(SName,WNumber,PNumber,Command): #{{{
     return call(
             'tmux send-keys -t '+
             re.escape(SName)+':'+str(WNumber)+'.'+str(PNumber)+' '+
             re.escape(Command)+' C-m',shell=True)
 
-def SetLayout(SName,WNumber,Layout):
+#}}}
+def SetLayout(SName,WNumber,Layout): #{{{
     call('tmux select-layout -t '+re.escape(SName)+':'+str(WNumber)+' tiled',shell=True)
     return call('tmux select-layout -t '+re.escape(SName)+':'+str(WNumber)+' '+re.escape(Layout),shell=True)
 
+#}}}
 
-def SaveSessions(File):
-    pass
+# Creation Functions
+def CreatePanesByModel(SName,WName,WNumber,Root,Model,SpecificDir): #{{{
+    Layout = Model.get('layout') or Model.get('Layout')
+    Panes  = Model.get('panes')  or Model.get('Panes')
+    Dir = None
+    if SpecificDir is None:
+        Dir    = Model.get('dir')    or Model.get('Dir')
+        if Dir is not None:
+            Dir = Dir.replace('<WName>',WName)
+            Dir = Dir.replace('<WindowName>',WName)
+            Dir = Dir.replace('<SName>',SName)
+            Dir = Dir.replace('<SessionName>',SName)
+    else:
+        Dir = SpecificDir
 
-def LoadSessions(File):
-    # Load YAML File as Python Object
-    Stream = open(File,'r')
-    Sessions = load(Stream)
-    Stream.close()
-    SNumber = 0
+    if Root is not None:
+        Directory = os.path.expanduser(Root)+'/'+str(Dir)
+    else:
+        Directory = Dir
 
+    print('    --> Window: '+WName)
 
-    call('clear',shell=True)
+    for PNumber in range(len(Panes)):
+        Command = Panes[PNumber]
+        NewPane(SName,WNumber,PNumber,Command,Directory)
+
+    SetLayout(SName,WNumber,Layout)
+
+#}}}
+def CreateWindows(SName,Root,Models,Windows): #{{{
+    if Models == None:
+        print('Without Models')
+    else:
+        #print('With Models')
+        for WNumber in range(len(Windows)):
+            Window = Windows[WNumber]
+            WName = Window[0]
+            WModel = Window[1]
+            SpecificDir = None
+            if len(Window) == 3:
+                SpecificDir = Window[2]
+
+            Model = Models[WModel]
+            NewWindow(SName,WName,WNumber)
+            CreatePanesByModel(SName,WName,WNumber,Root,Model,SpecificDir)
+
+#}}}
+def CreateSession(YAML): #{{{
+
+    SName   = YAML.get('name')    or YAML.get('Name')
+    Root    = YAML.get('root')    or YAML.get('Root')
+    Models  = YAML.get('models')  or YAML.get('Models')
+    Windows = YAML.get('windows') or YAML.get('Windows')
+
+    if Models == None:
+        LoadSessions(YAML)
+
     print()
+    print('  Creating Session: '+SName)
+    if not NewSession(SName):
+        CreateWindows(SName,Root,Models,Windows)
+
+
+#}}}
+def CreateSessions(FilesDirectory): #{{{
+    SNames = []
+    YAMLs = []
+    Files = system('ls '+FilesDirectory).split("\n")
+    for SNumber in range(len(Files)):
+        File = FilesDirectory+'/'+Files[SNumber]
+        Stream = open(File,'r')
+        try:
+            YAMLs.append(load(Stream))
+            SName = YAMLs[SNumber].get('name') or \
+                    YAMLs[SNumber].get('Name')
+            SNames.append(SName)
+        except:
+            pass
+        Stream.close()
+
     print()
+    print('Select The Sessions [0 1 2 ...]:')
+    print()
+    for SNumber in range(len(SNames)):
+        print('    ['+str(SNumber)+'] '+SNames[SNumber])
 
-    for Session in Sessions:
-        for SName in Session.keys():
-            Session = Session[SName]
-            Root = Session.get('root')
-            Windows = Session.get('windows')
-            WNumber = 0
+    print()
+    Option = input('Create All Sessions [Enter To Chose All]?')
+    call('clear')
 
-            ThereIsSession = NewSession(SName)
+    SessionsToLoad = []
+    if Option == '':
+        SessionsToLoad = range(len(SNames))
+    else:
+        for Choice in Option.strip().split(' '):
+            if Choice and (not Choice in SessionsToLoad) and Choice.isdigit():
+                SessionsToLoad.append(Choice)
 
-            if not ThereIsSession:
-                print('--> Session: '+SName+' [...]')
-                for Window in Windows:
-                    for WName in Window:
-                        Window = Window[WName]
-                        Panes  = Window.get('panes')
-                        Layout = Window.get('layout')
-                        Dir    = Window.get('dir')
-                        PNumber = 0
+    for SNumber in SessionsToLoad:
+        SNumber = int(SNumber)
+        if SNumber in range(len(SNames)):
+            CreateSession(YAMLs[SNumber])
 
-                        ThereIsWindow = NewWindow(SName,WName,WNumber)
+#}}}
 
-                        if not ThereIsWindow:
-                            
-                            for Pane in Panes:
-                                Command = Pane
-                                Directory = Root+'/'+Dir
-                                NewPane(SName,WNumber,PNumber,Command,Directory)
-                                PNumber += 1
+# Default Directory
+FilesDirectory = os.path.expanduser('~/.config/IniTmux')
 
-                        SetLayout(SName,WNumber,Layout)
-                        
-                    WNumber += 1
-
-        SNumber += 1
-    
-    #input('')
-    #call('killall tmux',shell=True)
-                    
-File = os.path.expanduser('~/.config/IniTmux/IniTmux.yml')
-
-LoadSessions(File)
+call('clear')
+print()
+print('---------------------------------------------')
+CreateSessions(FilesDirectory)
+print()
+print('Done. Type: "tmux attach-session" To Checkout your Sessions!')
