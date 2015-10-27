260c9abf26285ec9d2ad8b94b712a6628434a213
Itamar de Aguiar <itamar.de.aguiar@gmail.com>
2015-10-27 16:35:42 -0300

    Multiple Commands for Each Pane

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 
-# What is This Program?
+# What This Program is?
 
 This is just a session store/initializer for Tmux.
 
@@ -83,6 +83,125 @@ models:
         - ranger
         - watch du -sh .
 ```
+
+# What this 'models' attribute is?
+
+You can see the power of the `models` attribute, in the following example:
+
+* `~/.config/IniTmux/Packagers.yml`
+```yaml
+---
+name: Packagers
+windows:
+- [Pacman , two]
+- [Yaourt , two]
+- [npm    , two]
+- [gem    , two]
+- [pip    , two]
+models:
+    two:
+        layout: even-horizontal
+        panes: 2
+```
+
+with this configuration, IniTmux will create a session called `Packagers` with 5 windows. In each window, 
+IniTmux will apply the model called `two`, which will create 2 (`two`) panes without commands. 
+
+You can also see that both `root` and `dir` attributes wasn't written. In this case, IniTmux will change 
+directory to HOME (`~`).
+
+# YAML Syntax for IniTmux
+
+It is very simple. A YAML IniTmux file has up to 4 main attributes:
+
+### name or Name (String)
+
+```yaml
+name: <SessionName>
+```
+
+Is the name of the session. This attribute is obligatory. 
+
+### root or Root (String)
+
+```yaml
+root: <RootDirectory>
+```
+
+Is the directory root of all windows. This attribute is optional. If defined, its value will be concatenated 
+before the `dir`, which will be explained soon.
+
+Note: If you want to refer to `~`, please use `'~'` or `"~"` not only `~`. This symbol will be expanded to the 
+HOME location of the user who called the script. It uses `os.path.expanduser` of `os` python module.
+
+### models or Models
+
+TODO ...
+
+### windows or Windows (List of lists: [[],[],...,[]])
+
+Is the list of window names, model names and possible specific directory for all the panes of that window. This 
+attribute is obligatory. The skeleton is like the following:
+
+```yaml
+windows: 
+- [WindowName1,ModelName1 [,SpecificDir1]]
+# ...
+- [WindowNameN,ModelNameN [,SpecificDirN]]
+```
+
+The two first items of each window are mandatory. The third one is not. If setted, its value will overwrite the 
+`dir` attribute of the choosen model.
+
+Example:
+
+```yaml
+---
+name: Test
+root: '~/<SessionName>'
+windows:
+- [Downloads , two]
+- [Documents , two]
+- [Torrents  , two  , 'Downloads/<WindName>']    # <WindName> == 'Torrents'
+- [Books     , two  , 'Downloads/<WindowName>']  # <WindowName> == 'Books'
+
+models:
+    two:
+        layout: even-horizontal
+        dir: <WindowName>
+        panes:
+            - first:
+                - cd /i/server/http/<SName>/<WName>
+                - ranger
+            - df -h
+```
+
+So, this YAML file will create 4 windows. IniTmux will apply the model called `two` for each window. This model
+tells that the layout of each window will be `even-horizontal`. The full path of each window is little bit tricky,
+but it will follow the following rules:
+
+1) If SpecificDir is setted in some window, then `PATH = root+'/'+SpecificDir`. Exemple:
+    
+```text
+    Window: Torrents
+    PATH  : ~/Test/Downloads/Torrents
+```
+
+Note that `<SessionName>` and `<WindName>` were properly expanded.
+
+2) Else if SpecificDir isn't setted, then `PATH = root+'/'+dir`. Example:
+
+```text
+    Window: Documents
+    PATH  : ~/Test/Documents
+```
+
+In both cases, each window will have two panes. The first pane will first enter the location `/i/server/http/<SName>/<WName>`
+after proper expansions of `SName` and `WName`, and then execute `ranger`. The second pane will apply `df -h`.
+
+Once you can create panes with multiple commands (sequencially executed), you can use this approach to first enter some "Pane Specific"
+directory location that will overwrite all PATHs setted before.
+
 
 # Dependencies
 
--- src/main.py
+++ src/main.py
@@ -11,6 +11,15 @@ try:
 except ImportError:
     from yaml import Loader, Dumper
 
+WPattern = r"<(?i)(windown|wn|winn|windn)ame>"
+SPattern = r"<(?i)(sessionn|sn|sesn|sessn)ame>"
+TmuxLayouts = []
+TmuxLayouts.append('tiled')
+TmuxLayouts.append('even-horizontal')
+TmuxLayouts.append('even-vertical')
+TmuxLayouts.append('main-horizontal')
+TmuxLayouts.append('main-vertical')
+
 # Tmux Functions
 def NewSession(SName): #{{{
     return call('tmux new-session -s '+re.escape(SName)+' -d',shell=True)
@@ -40,22 +49,31 @@ def NewPane(SName,WNumber,PNumber,Command,Directory): #{{{
 
 #}}}
 def ChangeDir(SName,WNumber,PNumber,Directory): #{{{
-    return call(
-            'tmux send-keys -t '+
-            re.escape(SName)+':'+str(WNumber)+'.'+str(PNumber)+
-            ' "cd '+Directory+'" C-m',shell=True)
+    if Directory is None:
+        return
+    else:
+        return call(
+                'tmux send-keys -t '+
+                re.escape(SName)+':'+str(WNumber)+'.'+str(PNumber)+
+                ' "cd '+Directory+'" C-m',shell=True)
 
 #}}}
 def ApplyCommand(SName,WNumber,PNumber,Command): #{{{
-    return call(
-            'tmux send-keys -t '+
-            re.escape(SName)+':'+str(WNumber)+'.'+str(PNumber)+' '+
-            re.escape(Command)+' C-m',shell=True)
+    if Command is None:
+        return
+    else:
+        return call(
+                'tmux send-keys -t '+
+                re.escape(SName)+':'+str(WNumber)+'.'+str(PNumber)+' '+
+                re.escape(Command)+' C-m',shell=True)
 
 #}}}
 def SetLayout(SName,WNumber,Layout): #{{{
-    call('tmux select-layout -t '+re.escape(SName)+':'+str(WNumber)+' tiled',shell=True)
-    return call('tmux select-layout -t '+re.escape(SName)+':'+str(WNumber)+' '+re.escape(Layout),shell=True)
+    if Layout in TmuxLayouts:
+        return call('tmux select-layout -t '+re.escape(SName)+':'+str(WNumber)+' '+re.escape(Layout),shell=True)
+    else:
+        call('tmux select-layout -t '+re.escape(SName)+':'+str(WNumber)+' tiled',shell=True)
+        return call('tmux select-layout -t '+re.escape(SName)+':'+str(WNumber)+' '+re.escape(Layout),shell=True)
 
 #}}}
 
@@ -63,27 +81,55 @@ def SetLayout(SName,WNumber,Layout): #{{{
 def CreatePanesByModel(SName,WName,WNumber,Root,Model,SpecificDir): #{{{
     Layout = Model.get('layout') or Model.get('Layout')
     Panes  = Model.get('panes')  or Model.get('Panes')
-    Dir = None
+    Dir = ''
+
     if SpecificDir is None:
         Dir    = Model.get('dir')    or Model.get('Dir')
         if Dir is not None:
-            Dir = Dir.replace('<WName>',WName)
-            Dir = Dir.replace('<WindowName>',WName)
-            Dir = Dir.replace('<SName>',SName)
-            Dir = Dir.replace('<SessionName>',SName)
+            Dir = re.sub(WPattern,WName,Dir)
+            Dir = re.sub(SPattern,SName,Dir)
     else:
         Dir = SpecificDir
+        Dir = re.sub(WPattern,WName,Dir)
+        Dir = re.sub(SPattern,SName,Dir)
 
-    if Root is not None:
+    if   Root is None and Dir is None:
+        Directory = os.path.expanduser('~')
+
+    elif Root is None and Dir is not None:
+        Directory = os.path.expanduser(Dir)
+
+    elif Root is not None and Dir is None:
+        Root = re.sub(WPattern,WName,Root)
+        Root = re.sub(SPattern,SName,Root)
+        Directory = os.path.expanduser(Root)
+
+    elif Root is not None and Dir is not None:
+        Root = re.sub(WPattern,WName,Root)
+        Root = re.sub(SPattern,SName,Root)
         Directory = os.path.expanduser(Root)+'/'+str(Dir)
-    else:
-        Directory = Dir
 
     print('    --> Window: '+WName)
 
-    for PNumber in range(len(Panes)):
-        Command = Panes[PNumber]
-        NewPane(SName,WNumber,PNumber,Command,Directory)
+    if type(Panes) is int:
+        for PNumber in range(Panes):
+            Command = None
+            NewPane(SName,WNumber,PNumber,Command,Directory)
+    elif type(Panes) == type([]):
+        for PNumber in range(len(Panes)):
+            if type(Panes[PNumber]) == type({}):
+                Command = None
+                NewPane(SName,WNumber,PNumber,Command,Directory)
+                for Command in list(Panes[PNumber].values())[0]:
+                    Command = re.sub(WPattern,WName,Command)
+                    Command = re.sub(SPattern,SName,Command)
+                    ApplyCommand(SName,WNumber,PNumber,Command)
+            else:
+                Command = Panes[PNumber]
+                Command = re.sub(WPattern,WName,Command)
+                Command = re.sub(SPattern,SName,Command)
+                NewPane(SName,WNumber,PNumber,Command,Directory)
+
 
     SetLayout(SName,WNumber,Layout)
 
@@ -112,9 +158,6 @@ def CreateSession(YAML): #{{{
     Root    = YAML.get('root')    or YAML.get('Root')
     Models  = YAML.get('models')  or YAML.get('Models')
     Windows = YAML.get('windows') or YAML.get('Windows')
-
-    if Models == None:
-        LoadSessions(YAML)
 
     print()
     print('  Creating Session: '+SName)
